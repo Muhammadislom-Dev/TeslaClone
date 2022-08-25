d10177f3e9bcfb64ea518798b407fa7282b151ec
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-08-26 00:43:11 +0500

    add

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import { Sidebar } from "./Sidebar";
 
 const Navbar = () => {
+  const [sidebar, setSidebar] = useState(false);
 
-    const [sidebar, setSidebar] = useState(false)
-
-    const showSidebar = () => setSidebar(!sidebar)
-
+  const showSidebar = () => setSidebar(!sidebar);
 
   return (
     <nav className={styles.navbar}>
@@ -23,54 +21,66 @@ const Navbar = () => {
       </Link>
       <ul className={styles.navbarList}>
         <li className={styles.items}>
-          <a  className={styles.itemLink} href="#">Model S</a>
+          <a className={styles.itemLink} href="#">
+            Model S
+          </a>
         </li>
         <li className={styles.items}>
-          <a className={styles.itemLink} href="#">Model 3</a>
+          <a className={styles.itemLink} href="#">
+            Model 3
+          </a>
         </li>
         <li className={styles.items}>
-          <a className={styles.itemLink} href="#">Model X</a>
+          <a className={styles.itemLink} href="#">
+            Model X
+          </a>
         </li>
         <li className={styles.items}>
-          <a className={styles.itemLink} href="#">Model Y</a>
+          <a className={styles.itemLink} href="#">
+            Model Y
+          </a>
         </li>
         <li className={styles.items}>
-          <a className={styles.itemLink} href="#">Solar Roof</a>
+          <a className={styles.itemLink} href="#">
+            Solar Roof
+          </a>
         </li>
         <li className={styles.items}>
-          <a className={styles.itemLink} href="#">Solar Panels</a>
+          <a className={styles.itemLink} href="#">
+            Solar Panels
+          </a>
         </li>
       </ul>
       <div className={styles.menu}>
         <li className={styles.items}>
-          <a className={styles.itemLink} href="#">Shop</a>
+          <a className={styles.itemLink} href="#">
+            Shop
+          </a>
         </li>
         <li className={styles.items}>
-          <a className={styles.itemLink} href="#">Account</a>
+          <a className={styles.itemLink} href="#">
+            Account
+          </a>
         </li>
-        <li className={styles.items} onClick={showSidebar} >
-             Menu
+        <li className={styles.items} onClick={showSidebar}>
+          Menu
         </li>
       </div>
 
-      <div className={sidebar ? 'side-menu active' : 'side-menu'}>
-
+      <div className={sidebar ? "side-menu active" : "side-menu"}>
         {/* <CloseIcon onClick={showSidebar} className='close-icon' /> */}
-          <ul className="sidebarNav">
-            {
-              Sidebar.map((item, index) => {
-                return(
-                  <li key={index} className="sidebar-item" >
-                    <Link className="sidebar-link" href={item.path}>
-                        <a>{item.title}</a>
-                    </Link>
-                  </li>
-                )
-              })
-            }
-          </ul>
+        <ul className="sidebarNav">
+          {Sidebar.map((item, index) => {
+            return (
+              <li key={index} className="sidebar-item">
+                <Link className="sidebar-link" href={item.path}>
+                  <a>{item.title}</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-
     </nav>
   );
 };
